35748c6047e1c906f1a925c4da8f8ee7d39998ee
John Sullivan <sulljohn12@gmail.com>
2019-04-30 14:56:27 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CS52 Workshops:  Cool APIs!
+
+*Completed by John Sullivan 4/30/2019*
+
+Surge URL: [Link](https://sulljohn-cs52-material.surge.sh/)
---
+++ app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.scss';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('main'));
